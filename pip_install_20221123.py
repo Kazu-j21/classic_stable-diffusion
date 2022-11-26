612072f22854d9702c3deec6bdf94c0702a548e0
cocoa <cocoa@cocoa-air.local>
2022-11-26 17:12:03 +0900

    first commit

---
+++ pip_install_20221123.py
@@ -0,0 +1,17 @@
+pip install albumentations==0.4.3
+pip install diffusers
+pip install opencv-python==4.1.2.30
+pip install pudb==2019.2
+pip install imageio==2.9.0
+pip install imageio-ffmpeg==0.4.2
+pip install pytorch-lightning
+pip install omegaconf==2.1.1
+pip install test-tube>=0.7.5
+pip install streamlit>=0.73.1
+pip install einops==0.3.0
+pip install torch-fidelity==0.3.0
+pip install transformers==4.19.2
+pip install kornia
+pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
+pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
+pip install invisible-watermark
